0564ea58249a12f8183f5e58a1cdd16737fa1801
JethroNatividad <natividad.jet@gmail.com>
2024-02-02 11:31:56 +0800

    write pseudocode

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,15 @@
 // Output: the name
 
 fn main() {
+    // Initialize empty vector
+
+    // loop
+    // get input_name
+    // if input is blank, stop loop.
+
+    // if not blank, push to vector.
+
+    // select random in vector
+    // print, "The winner is... {}"
     println!("Hello, world!");
 }
