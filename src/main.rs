855eb80c628ffa0c97a693497b209d88c95a5ef9
JethroNatividad <natividad.jet@gmail.com>
2024-02-02 11:49:17 +0800

    add name

--- src/main.rs
+++ src/main.rs
@@ -27,10 +27,13 @@ fn main() {
     let mut names: Vec<String> = vec![];
 
     // loop
-    // get input_name
-    // if input is blank, stop loop.
+    loop {
+        // get input_name
+        let name: String = get_input("Enter a name: ");
+        // if input is blank, stop loop.
 
-    // if not blank, push to vector.
+        // if not blank, push to vector.
+    }
 
     // select random in vector
     // print, "The winner is... {}"
