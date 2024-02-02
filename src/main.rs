3ad528248638ebea400a57d78f304296e580b5a9
JethroNatividad <natividad.jet@gmail.com>
2024-02-02 11:47:47 +0800

    implement get_input

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,26 @@
 // Process: pick random
 // Output: the name
 
+fn get_input<T: std::str::FromStr>(prompt: &str) -> T {
+    loop {
+        print!("{}", prompt);
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+
+        match input.trim().parse() {
+            Ok(value) => break value,
+            Err(_) => println!("Invalid input. Please try again."),
+        }
+    }
+}
+
 fn main() {
     // Initialize empty vector
+    let mut names: Vec<String> = vec![];
 
     // loop
     // get input_name
@@ -14,5 +32,4 @@ fn main() {
 
     // select random in vector
     // print, "The winner is... {}"
-    println!("Hello, world!");
 }
